781e84694d4e6010fa5bc5675dc9a6d13a634334
Documenter.jl <documenter@juliadocs.github.io>
2022-11-30 11:37:01 +0000

    build based on 63c5be2

---
+++ v0.1.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DSForces","category":"page"},{"location":"#DSForces","page":"Home","title":"DSForces","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DSForces.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DSForces]","category":"page"}]
+}
